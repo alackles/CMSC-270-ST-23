82a80af6ba744ff4918e6f05fccdc3d75d7a23b8
Acacia Ackles <alackles@protonmail.com>
2023-04-06 14:08:48 -0500

    Remove grades

--- grading/hwk0/Dice(1)/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Dice dice = new Dice();
-        dice.setSizeNum(6);
-        dice.roll();
-    }
-}
--- grading/hwk0/Die/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Dice dice = new Dice(20);
-        System.out.println(dice.roll());
-        System.out.println(dice.roll());
-        System.out.println(dice.roll());
-    }
-}
